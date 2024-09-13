2358f07988c31f29be0431924412296fd16783df
moseremzy <agbaojemoses@gmail.com>
2024-09-13 15:22:50 -0700

    Update fetch_details.php

--- server/fetch_details.php
+++ server/fetch_details.php
@@ -1,7 +1,5 @@
 <?php
 
-header('Content-Type: application/json');
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
 $all_months = array(
@@ -360,6 +358,8 @@ while($counter < count($zodiac_info)) {
 
     'zodiac_info' =>  get_zodiac_info(),
 );
+   
+    header('Content-Type: application/json');
 
     $response = json_encode($data);
 
@@ -368,6 +368,9 @@ while($counter < count($zodiac_info)) {
     exit();
 
 } else {
+
+    
+   header('Content-Type: application/json');
 
     http_response_code(405);
 
